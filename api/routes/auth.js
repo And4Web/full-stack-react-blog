fcac72d8794742966de7e0d25dcffa8b0e33bfa5
And4Web <anands2807@gmail.com>
2022-05-13 13:32:29 +0530

    auth and registering route creadted and functional peoperly

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //register:
+
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -23,5 +24,21 @@ router.post("/register", async (req, res) => {
 });
 
 //login:
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(400).json("Wrong Credentials!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong Credentials!");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
