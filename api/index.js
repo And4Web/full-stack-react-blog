13c10f77c9a42b391e6a24b3291a3965cf66e1c3
And4Web <anands2807@gmail.com>
2022-05-13 12:39:27 +0530

    auth route has been created successfully, now bcrypting the password

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
-const PORT = 8000;
 const dotEnv = require("dotenv");
 
+const authRoute = require("./routes/auth");
+
+const PORT = 8000;
+
+const app = express();
+
 dotEnv.config();
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to Mongo"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("home page");
